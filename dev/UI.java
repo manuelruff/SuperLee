4ed6f26186d756827dcae9930efddc10b92dad23
roee <roeeyls1234@gmal.com>
2023-04-04 20:37:02 +0300

    Order functions + CLI

--- dev/UI.java
+++ dev/UI.java
@@ -5,12 +5,13 @@ public class UI {
         char choice ='a';
 
         while(choice != '0')
-        System.out.println("Shipping Menu:");
+        System.out.println("Shipping Main Menu:");
         System.out.println("1 - Site Menu");
         System.out.println("2 - Order Menu");
         System.out.println("3 - Shipping Menu");
         System.out.println("4 - Drivers Menu");
-
+        System.out.println(("5 - Truck Menu"));
+        System.out.println("6 - ");
     }
 
 
