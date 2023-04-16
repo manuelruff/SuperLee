01e99297ca6ccaebf98d840389fdfb291fc4792e
Manuel Ruffieux <manuel42994299@gmail.com>
2023-04-16 18:45:42 +0300

    Update HRService.java

--- dev/adss2020_v01/src/Service/HRService.java
+++ dev/adss2020_v01/src/Service/HRService.java
@@ -1,4 +1,15 @@
 package Service;
 
+//we will add here functions that the other modules want from us!
 public class HRService {
+    //for model transportation
+
+    //add a driver to the shift
+    public boolean AddDriver(){
+        return false;
+    }
+
+
+
 }
+
