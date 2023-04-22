cb8ad99bb79c5584c8ad906317b1cb5fd712a9b1
Manuel Ruffieux <manuel42994299@gmail.com>
2023-04-22 22:48:13 +0300

    added database conection

---
+++ dev/adss2020_v01/src/DataAccess/Connect.java
@@ -0,0 +1,39 @@
+package DataAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+           String url = "jdbc:sqlite:dev/adss2020_v01/resource/HRDB/HRDB"; 
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+}
