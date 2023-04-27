d1335a99998e9f7c05a72514a4d5405c4a02f8c9
OmriPere <peretz711@gmail.com>
2023-04-27 20:28:03 +0200

    # shift hours

--- dev/adss2020_v01/src/DataAccess/SuperMapper.java
+++ dev/adss2020_v01/src/DataAccess/SuperMapper.java
@@ -1,8 +1,5 @@
 package DataAccess;
-import Domain.Days;
-import Domain.ManagerController;
-import Domain.Super;
-import Domain.Worker;
+import Domain.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -50,7 +47,7 @@ public class SuperMapper {
                 //load his cash registers to the mapper of the cash register
                 CashRegisterMapper.PutCashRegister(name,SuperMap.get(name).get_cash_register());
                 //rad the times of shift in each day
-
+                ReadSuperTime(name);
                 //read the weekly
 
             }
@@ -60,6 +57,30 @@ public class SuperMapper {
         }
     }
 
+    private static void ReadSuperTime(String BranchName){
+        double sm,em,se,ee;
+        String day;
+        try {
+            java.sql.Statement stmt = conn.createStatement();
+            java.sql.ResultSet rs = stmt.executeQuery("select * from SuperShiftsTime WHERE SuperName=="+BranchName+"" );
+            while (rs.next()){
+                sm=rs.getDouble("StartMorning");
+                em=rs.getDouble("EndMorning");
+                se=rs.getDouble("StartEvening");
+                ee=rs.getDouble("EndEvening");
+                day = rs.getString("Day");
+                SuperMap.get(BranchName).setStart_morning(Days.valueOf(day),sm);
+                SuperMap.get(BranchName).setEnd_morning(Days.valueOf(day),em);
+                SuperMap.get(BranchName).setStart_evening(Days.valueOf(day),se);
+                SuperMap.get(BranchName).setEnd_evening(Days.valueOf(day),ee);
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("i have a with Read super time problem sorry");
+        }
+    }
+
+
     public static void WriteAllSupers() {
         for (Super branch : SuperMap.values()) {
             String name;
@@ -68,9 +89,27 @@ public class SuperMapper {
                 name = branch.getName();
                 stmt.executeUpdate("INSERT OR IGNORE INTO Super (name) VALUES ('" + name + "')");
                 //write the shifts
+                // write the super hours
+                WriteSuperTime(branch.getName());
             } catch (SQLException e) {
                 System.out.println("i have a problem in writing the super sorry");
             }
+        }
+    }
+    private static void WriteSuperTime(String BranchName){
+        try {
+            java.sql.Statement stmt = conn.createStatement();
+            Super curr = SuperMap.get(BranchName);
+            for (Days day : Days.values()) {
+                double sm = curr.getStart_morning(day);
+                double em = curr.getEnd_morning(day);
+                double se = curr.getStart_evening(day);
+                double ee = curr.getEnd_evening(day);
+                stmt.executeUpdate("INSERT OR IGNORE INTO SuperShiftsTime (SuperName, Day, StartMorning, EndMorning, StartEvening, EndEvening) VALUES ('" + BranchName + "', '" + day.toString() + "', '" + sm + "', '" + em + "', '" + se + "', '" + ee + "')");
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("i have a problem in writing the super time sorry");
         }
     }
 
