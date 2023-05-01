5a8eb3a7f160cc30371acd10990f9a5033cd12fa
Manuel Ruffieux <manuel42994299@gmail.com>
2023-05-01 11:29:15 +0300

    moved stuff

--- dev/Main.java
+++ dev/Main.java
@@ -1,9 +1,11 @@
-
+import HR.Presentation.GeneralUI;
 
 public class Main {
     public static void main(String[] args) {
     UI ui = new UI();
     //ui.shippingMenu();
 
+    //calls the GeneralUI object starting function
+    GeneralUI.StartMe();
     }
 }
--- dev/adss2020_v01/src/Main.java
+++
@@ -1,8 +0,0 @@
-import HR.Presentation.GeneralUI;
-
-public class Main {
-    public static void main(String[] args) {
-        //calls the GeneralUI object starting function
-        GeneralUI.StartMe();
-    }
-}
