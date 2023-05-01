3c7d003a5926e457c940b262925c04d917a56dc0
Manuel Ruffieux <manuel42994299@gmail.com>
2023-05-01 11:33:37 +0300

    moved stuff

--- dev/Days.java
+++ dev/Days.java
@@ -5,4 +5,5 @@ public enum Days {
     Wednesday,
     Thursday,
     Friday
+
 }
--- dev/Main.java
+++ dev/Main.java
@@ -2,7 +2,7 @@ import HR.Presentation.GeneralUI;
 
 public class Main {
     public static void main(String[] args) {
-    UI ui = new UI();
+//    UI ui = new UI();
     //ui.shippingMenu();
 
     //calls the GeneralUI object starting function
