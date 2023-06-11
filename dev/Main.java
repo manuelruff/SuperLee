d4bbeb7ecdff67cecc1d563654d2854b71b6b1b8
roeey1 <roeeyls1234@gmail.com>
2023-06-11 13:17:11 +0300

    test

--- dev/Main.java
+++ dev/Main.java
@@ -26,6 +26,7 @@ public class Main {
                 new HRManager();
             }
             // else if
+            //shipment manager
             else{
                 JOptionPane.showMessageDialog(null,"Wrong role");
                 System.exit(0);
