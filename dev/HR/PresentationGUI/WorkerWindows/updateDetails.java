1ead1dc79cc66a200383b37f72be7e7f3c051c6c
Manuel Ruffieux <manuel42994299@gmail.com>
2023-06-12 02:22:53 +0300

    update employee window done

--- dev/HR/PresentationGUI/WorkerWindows/updateDetails.java
+++ dev/HR/PresentationGUI/WorkerWindows/updateDetails.java
@@ -3,6 +3,11 @@ import HR.Bussiness.WorkerController;
 import HR.PresentationGUI.WorkerGUI;
 import javax.swing.*;
 import javax.swing.JOptionPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,62 +15,110 @@ import java.util.List;
 
 public class updateDetails extends JFrame implements ActionListener {
     private JPanel updateDetailsWin;
-    private JTextField password,bank,name;
+    private JTextField password, bank, name;
     private JButton updateButton;
     private JButton backButton;
-    //save the preview window to open in the end
+    // save the preview window to open in the end
     private WorkerGUI save;
-    //take instance for worker controller
+    // take instance for worker controller
     private WorkerController workerController;
-    //save the old values to know what to update
+    // save the old values to know what to update
     private String Wid;
     private String Wname;
     private String Wpass;
     private String Wbank;
-    public updateDetails(WorkerGUI save,List<String> worker) {
-        this.save=save;
+
+    public updateDetails(WorkerGUI save, List<String> worker) {
+        this.save = save;
         this.setContentPane(updateDetailsWin);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setMinimumSize(new Dimension(300,200));
+        this.setMinimumSize(new Dimension(300, 200));
         this.pack();
         this.setVisible(true);
         this.setTitle("Worker");
-        //add listeners
+
+        // Add listeners
         updateButton.addActionListener(this);
         backButton.addActionListener(this);
         this.workerController = WorkerController.getInstance();
-        //put current values and save them
-        this.Wid=worker.get(0);
-        this.Wname=worker.get(1);
-        this.Wpass=worker.get(2);
-        this.Wbank=worker.get(3);
 
+        // Put current values and save them
+        this.Wid = worker.get(0);
+        this.Wname = worker.get(1);
+        this.Wpass = worker.get(2);
+        this.Wbank = worker.get(3);
+
+        // Apply document filters
+        bank.setDocument(new IntegerDocument());
+
+        // Sets the text boxes with first info
+        this.name.setText(this.Wname);
+        this.password.setText(this.Wpass);
+        this.bank.setText(this.Wbank);
+
+        // Add document listeners to text fields to enable/disable the update button
+        name.getDocument().addDocumentListener(new UpdateButtonDocumentListener());
+        password.getDocument().addDocumentListener(new UpdateButtonDocumentListener());
+        bank.getDocument().addDocumentListener(new UpdateButtonDocumentListener());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==updateButton){
-            //change password if changed
-            if(!(password.getText().equals(this.Wpass)))
-            {
-                workerController.ChangeWorkerPassword(this.Wid,password.getText());
+        if (e.getSource() == updateButton) {
+            // Change password if changed
+            if (!(password.getText().equals(this.Wpass))) {
+                workerController.ChangeWorkerPassword(this.Wid, password.getText());
             }
-            //change name if changed
-            if(!(name.getText().equals(this.Wname)))
-            {
-                workerController.ChangeWorkerName(this.Wid,name.getText());// change name
+            // Change name if changed
+            if (!(name.getText().equals(this.Wname))) {
+                workerController.ChangeWorkerName(this.Wid, name.getText());
             }
-            //changed bank if changed
-            if(!(bank.getText().equals(this.Wbank)))
-            {
-                workerController.ChangeWorkerBank(this.Wid,Integer.parseInt(bank.getText()));
+            // Change bank if changed
+            if (!(bank.getText().equals(this.Wbank))) {
+                workerController.ChangeWorkerBank(this.Wid, Integer.parseInt(bank.getText()));
             }
-            //todo make sure that bank is only a number and that all is filled when he wants to press update
-        }
-        else if(e.getSource()==backButton)
-        {
+            // Todo: make sure that bank is only a number and that all fields are filled when the update button is pressed
+        } else if (e.getSource() == backButton) {
             this.dispose();
             save.setVisible(true);
+        }
+    }
+
+    // Inner class for document filter
+    class IntegerDocument extends PlainDocument {
+        @Override
+        public void insertString(int offset, String s, AttributeSet attributeSet) throws BadLocationException {
+            if (s == null) {
+                return;
+            }
+            try {
+                Integer.parseInt(s);
+                super.insertString(offset, s, attributeSet);
+            } catch (NumberFormatException e) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+
+    // Inner class for document listener to enable/disable the update button
+    class UpdateButtonDocumentListener implements DocumentListener {
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            updateButton.setEnabled(isFieldsFilled());
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            updateButton.setEnabled(isFieldsFilled());
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            updateButton.setEnabled(isFieldsFilled());
+        }
+
+        private boolean isFieldsFilled() {
+            return !name.getText().isEmpty() && !password.getText().isEmpty() && !bank.getText().isEmpty();
         }
     }
 }
